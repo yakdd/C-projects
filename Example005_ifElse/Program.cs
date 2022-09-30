c8cb5d24a29e34df27027ee4a89317d1aa0e4bfc
yakdd <yakdd@yandex.ru>
2022-09-30 17:41:03 +0300

    added condition

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// exmp005 if else
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("О, да это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
