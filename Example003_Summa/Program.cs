eaa88a0f1e4520bef3042305fd108f714fec43eb
yakdd <yakdd@yandex.ru>
2022-09-30 17:22:24 +0300

    add random

--- Example003_Summa/Program.cs
+++ Example003_Summa/Program.cs
@@ -1,6 +1,6 @@
 ﻿// exmp_003 Summa
-int numberA = 3;
-int numberB = 6;
+int numberA = new Random().Next(1, 10);     // [1,10)
+int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberA + "+" + numberB + "=" + numberA + numberB);   // 3+6=36
 int summ = numberA + numberB;
 Console.WriteLine(numberA + "+" + numberB + "=" + summ);                // 3+6=9
