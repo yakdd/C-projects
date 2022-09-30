1ba7504f82e6536f3a8ce90acee2f9c7dd60cd41
yakdd <yakdd@yandex.ru>
2022-09-30 17:50:21 +0300

    added max number

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿// exmp006 max number
+int a = 1;
+int b = 2;
+int c = 61;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
