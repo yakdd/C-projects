7ecefd8dae7d64abf6da09ea4d78d2bd74f39cd2
yakdd <yakdd@yandex.ru>
2022-10-04 19:30:00 +0300

    2 lecture

---
+++ Example008_MaxOf9/Program.cs
@@ -0,0 +1,27 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 44;
+int c1 = 75;
+
+int a2 = 11;
+int b2 = 771;
+int c2 = 21;
+
+int a3 = 14;
+int b3 = 5;
+int c3 = 200;
+
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,55 @@
+﻿// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+void FillArray(int[] collection)    // void - метод (функция), который ничего не возвращает
+{                                   // также называется процедурой
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{coll[position]} ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int length = collection.Length;
+    int index = 0;
+    int posFind = -1;   // в случае отсутсвия икомого элемента в массиве выдаст -1.
+    while (index < length)
+    {
+        if (collection[index] == find)
+        {
+            posFind = index;
+            break;
+        }
+        index++;
+    }
+    return posFind;
+}
+
+int[] array = new int[10]; // создать новый массив с 10 элементами по-умолчанию
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+// Console.WriteLine(array);
+
+int pos = IndexOf(array, 15);
+Console.WriteLine(pos);
