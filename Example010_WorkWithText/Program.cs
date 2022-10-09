db3e1221f84836aba626b515e67de1459f0b8b57
yakdd <yakdd@yandex.ru>
2022-10-09 14:36:44 +0300

    3 lecture

---
+++ Example010_WorkWithText/Program.cs
@@ -0,0 +1,28 @@
+﻿string ReplaceSymbol(string text, char oldSym, char newSym)
+{
+    string result = String.Empty;      // создает новую пустую строку
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == oldSym) result += $"{newSym}";
+        else result += $"{text[i]}";
+    }
+    return result;
+}
+
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?";
+
+Console.WriteLine(text);
+Console.WriteLine();
+
+string newText = ReplaceSymbol(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = ReplaceSymbol(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = ReplaceSymbol(text, 'с', '$');
+Console.WriteLine(newText);
---
+++ Example011_SortArray/Program.cs
@@ -0,0 +1,52 @@
+﻿// int[] array = { 8, 7, 6, 5, 4, 3, 2, 1 };
+// int[] array = { 0, 8, 4, 7, 6, 5, 0, 7, 12, 4, 3, 2, 1 };
+
+int[] FillArray(int count)
+{
+    int[] createArray = new int[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        createArray[i] = new Random().Next(0, 100);
+    }
+
+    return createArray;
+}
+
+
+int[] SortArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int beginPosition = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[i];
+                arr[beginPosition] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+
+int[] array = FillArray(10);
+PrintArray(array);
+Console.WriteLine();
+
+SortArray(array);
+PrintArray(array);
