a679eba2c95a4332036463e61009107e55a68137
yakdd <yakdd@yandex.ru>
2023-01-13 15:13:38 +0300

    Last commit

--- Example012_TwoDimensionalArray/Program.cs
+++ Example012_TwoDimensionalArray/Program.cs
@@ -10,8 +10,9 @@ for (int rows = 0; rows < 2; rows++)
 {
     for (int columns = 0; columns < 5; columns++)
     {
-        Console.WriteLine($"-{table[rows, columns]}-");
+        Console.Write($"-{table[rows, columns]}-|");
     }
+    Console.WriteLine();
 }
 Console.WriteLine();
 
